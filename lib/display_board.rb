7e4a3eb598e8b657481572bd57b5e1ea05f4071c
Gabriel Martin <gabriel.martin423@gmail.com>
2020-08-20 13:10:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(arg)
+
+def display_board(move)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
+  
+  
+display_board
