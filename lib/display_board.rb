9f2ec30ddd90b9b90a3b3e90a7a7fab557f398cf
Gabriel Martin <gabriel.martin423@gmail.com>
2020-08-20 13:29:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,16 +1,2 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-
-def display_board(move)
-  rows = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
-  puts rows[0]
-  puts rows[1]
-  puts rows[2]
-  puts rows[3]
-  puts rows[4]
-end
-board = [" "," "," "," "," "," "," "," "," "]
-display_board(board)
-
-rows[2] = "   | X |   "
-display_board(board)
